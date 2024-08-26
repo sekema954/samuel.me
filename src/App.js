fdc7051ec5b0aa0d893a7b3f50b91d648cb9334a
sekema954 <sekema954@gmail.com>
2024-08-26 17:11:52 -0400

    Navbar and start page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import StartPage from './Pages/StartPage';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar></Navbar>
+        <div className="App"></div>
+        <Routes>
+          <Route path='/home' element={<HomePage></HomePage>}></Route>
+          <Route path='/' element={<StartPage></StartPage>}></Route>
+        </Routes>
+
+    </Router>
   );
 }
 
