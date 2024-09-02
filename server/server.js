63bf9ab54c83c9cc2d24c5da0ddf1853fd5ad5e6
sekema954 <sekema954@gmail.com>
2024-09-01 20:38:02 -0400

    test

--- server/server.js
+++ server/server.js
@@ -31,5 +31,5 @@ app.get('*', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/projects`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
